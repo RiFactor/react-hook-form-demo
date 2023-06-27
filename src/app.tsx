85850100573aa04c8cd2a3e5d4a319989c712b91
Ri <ri@3sidedcube.com>
2023-06-27 13:26:28 +0100

    instead of useState

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,24 @@
-import { FC } from "react";
+import React, { FC } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 export interface IProps {}
 
 const App: FC<IProps> = props => {
   const {} = props;
 
-  return <div>Hello World</div>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit: SubmitHandler<any> = data => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" {...register("firstName")}></input>
+      <input type="text" {...register("lastName")}></input>
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default App;
